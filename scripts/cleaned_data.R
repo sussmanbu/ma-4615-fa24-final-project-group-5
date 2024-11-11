3d01d89272de65199fec38412de377b9dd66fae9
Emily Lin <lyriem@bu.edu>
2024-11-11 18:14:50 -0500

    data.qmd update

--- scripts/cleaned_data.R
+++ scripts/cleaned_data.R
@@ -7,17 +7,10 @@ employment_data <- readRDS("Employment2017.rds")
 cleaned_data <- employment_data %>%
   select(
     Nation, Region, Division, State, CBSA, County, NAICS2, NAICS2_Name, NAICS3, NAICS3_Name, Establishments,
-    matches("^WHT_([1-9]|1_2)$"),
-    matches("^BLKT_([1-9]|1_2)$"),
-    matches("^HISPT_([1-9]|1_2)$"),
-    matches("^ASIANT_([1-9]|1_2)$"),
-    matches("^AIANT_([1-9]|1_2)$"),
-    matches("^NHOPIT_([1-9]|1_2)$"),
-    matches("^TOMRT_([1-9]|1_2)$"),
-    matches("^MT_([1-9]|1_2)$"),
-    matches("^FT_([1-9]|1_2)$")
+    matches("^(WHT|BLKT|HISPT|ASIANT|AIANT|NHOPIT|TOMRT|MT|FT)[1-9](1_2)?$")
   )
 
+# Ensure the directory exists, then save the cleaned data
 if (!dir.exists("../ma-4615-fa24-final-project-group-5/dataset")) {
   dir.create("../ma-4615-fa24-final-project-group-5/dataset", recursive = TRUE)
 }
