07113538a4aeed5389fde52e13ba5bdf59e1a48b
Emily Lin <lyriem@bu.edu>
2024-12-18 16:16:49 -0500

    Built site for gh-pages

--- scripts/cleaned_data.R
+++ scripts/cleaned_data.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(tidyselect)
 library(here)
 
+#2017
 employment_data <- readRDS("Employment2017.rds")
 
 cleaned_data <- employment_data %>%
@@ -10,9 +11,27 @@ cleaned_data <- employment_data %>%
     matches("^(WHT|BLKT|HISPT|ASIANT|AIANT|NHOPIT|TOMRT|MT|FT)[1-9](1_2)?$")
   )
 
-# Ensure the directory exists, then save the cleaned data
-if (!dir.exists("../ma-4615-fa24-final-project-group-5/dataset")) {
-  dir.create("../ma-4615-fa24-final-project-group-5/dataset", recursive = TRUE)
+if (!dir.exists("../ma-4615-fa24-final-project-group-5/scripts")) {
+  dir.create("../ma-4615-fa24-final-project-group-5/scripts", recursive = TRUE)
 }
 
-saveRDS(cleaned_data, "../ma-4615-fa24-final-project-group-5/dataset/Cleaned_Employment2017.rds")
+saveRDS(cleaned_data, "../ma-4615-fa24-final-project-group-5/scripts/Cleaned_Employment2017.rds")
+
+
+#2022
+employment_2022 <- readRDS("dataset/EEO1_2022_PUF.rds")
+
+employment_2022 <- employment_2022 %>%
+  rename_with(toupper)
+
+cleaned_2022 <- employment_2022 %>%
+  select(
+    NATION, REGION, DIVISION, STATE, CBSA, COUNTY, NAICS2, NAICS2_NAME, NAICS3, NAICS3_NAME, ESTABLISHMENTS,
+    matches("^(WHT|BLKT|HISPT|ASIANT|AIANT|NHOPIT|TOMRT|MT|FT)[1-9](_2)?$")
+  )
+
+if (!dir.exists("../ma-4615-fa24-final-project-group-5/scripts")) {
+  dir.create("../ma-4615-fa24-final-project-group-5/scripts", recursive = TRUE)
+}
+
+saveRDS(cleaned_2022, "../ma-4615-fa24-final-project-group-5/scripts/Cleaned_Employment2022.rds")
